923eee8c643e822f9d8c3cd9f82c8022946e7fc0
Jhemeson Silva Mota <jhemesonmotta@crptecnologia.com.br>
2018-11-06 10:59:23 -0300

    relacionamento produtosPreferidos/ClientesQueOPreferem

--- grails-app/domain/helloworld/Cliente.groovy
+++ grails-app/domain/helloworld/Cliente.groovy
@@ -6,9 +6,12 @@ class Cliente {
     String email
     String senha
     
-    static hasMany = [pedidos:Pedido]
-    // a nível de banco de dados, não é feito nada com o "hasMany", mas ele já identifica aqui,
+    static hasMany = [pedidos:Pedido, produtosPreferidos:Produto]
+    // pedidos
+        // a nível de banco de dados, não é feito nada com o "hasMany", mas ele já identifica aqui,
         // pelo groovy, a lista de pedidos do cliente
+    // produtosPreferidos representará uma relação many to many
+    
     static constraints = {
     }
 }
--- grails-app/domain/helloworld/Produto.groovy
+++ grails-app/domain/helloworld/Produto.groovy
@@ -7,6 +7,12 @@ class Produto {
     Estoque estoque
     // grails já entende que só é pra guardar o ID e manda essa infor para o hibernate
     
+    static hasMany = [clientesQuePreferem: Cliente]
+    // representará uma relação many to many
+    
+    static belongsTo = [Cliente]
+    // significa que "Cliente" é o lado mais forte.
+    
     static constraints = {
     }
 }
